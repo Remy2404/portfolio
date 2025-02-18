90453ed90ff81f2771c1096e89f5f6f2a3fa0b3d
Remy2404 <meemii191@gmail.com>
2025-02-19 02:19:31 +0700

    Update Vite configuration to exclude specific dependencies and enable CommonJS transformation

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['next-themes' , 'fraamer-motion', 'lucide-react']
+  },
   build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    },
     rollupOptions: {
       output: {
         manualChunks(id) {
