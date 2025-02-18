4200844fef0a5710c951050332cd7737a570e82b
Remy2404 <meemii191@gmail.com>
2025-02-19 02:48:49 +0700

    Update Vite configuration and package dependencies: remove unused plugins, upgrade framer-motion, and add glob dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
+import { defineConfig } from 'vite';
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      'react': 'react',
-    }
-  },
   build: {
     commonjsOptions: {
-      transformMixedEsModules: true,
+      transformMixedEsModules: true
     }
   }
-})
+});
